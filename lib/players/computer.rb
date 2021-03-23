2993477ba5cf3b3947255401e9ac321ea330f277
Shaina C <scailles23@gmail.com>
2021-03-23 03:41:09 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,30 +1,31 @@
 module Players
   class Computer < Player 
     
+ 
     
     
-    def move(board)
-      if board.cells[4] == " "
-        "5"
-      elsif board.cells[0] == " "
-        "1"
-      elsif board.cells[2] == " "
-        "3"
-      elsif board.cells[6] == " "
-        "7"
-      elsif board.cells[8] == " "
-        "9"
+  def move(board)
+    
+    if board.cells[0] == " "
+      "1"
+      
       elsif board.cells[1] == " "
-        "2"
+      "2"
+      elsif board.cells[2] == " "
+      "3"
       elsif board.cells[3] == " "
-        "4"
+      "4"
+      elsif board.cells[4] == " "
+      "5"
       elsif board.cells[5] == " "
-        "6"
+      "6"
+      elsif board.cells[6] == " "
+      "7"
       elsif board.cells[7] == " "
-        "8"
+      "8"
     end
   end
-    
-    
+  
+  
 end
 end
